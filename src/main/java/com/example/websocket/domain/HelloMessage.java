a89119309efafdd1d5d37f5b9148d5278c816846
jaemoon <woans5970@naver.com>
2024-04-12 22:31:58 +0900

    Create: HelloMessage.java

---
+++ src/main/java/com/example/websocket/domain/HelloMessage.java
@@ -0,0 +1,21 @@
+package com.example.websocket.domain;
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
