fac13efc07b89c6f5bda08056907160828d59ea3
jaemoon <woans5970@naver.com>
2024-04-12 22:32:07 +0900

    Create: WebSocketConfig.java

---
+++ src/main/java/com/example/websocket/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.example.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic"); //메시지 브로커로 바로 라우팅
+        registry.setApplicationDestinationPrefixes("/app"); //@MessageMapping URL로 라우팅됨
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket")
+                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
