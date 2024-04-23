74dc997b753553c5b0717978158e56234f9a4a6b
jaemoon <woans5970@naver.com>
2024-04-24 00:08:12 +0900

    Create: EnterAndExitRequestDto

---
+++ src/main/java/com/example/websocket/dto/v2/EnterAndExitRequestDto.java
@@ -0,0 +1,10 @@
+package com.example.websocket.dto.v2;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class EnterAndExitRequestDto {
+    private String userName;
+}
