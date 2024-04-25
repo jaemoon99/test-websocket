89ad6077fce781d1bdc946998da69ac962a2eb00
jaemoon <woans5970@naver.com>
2024-04-25 23:14:13 +0900

    Modify: ChatControllerV2

--- src/main/java/com/example/websocket/controller/ChatControllerV2.java
+++ src/main/java/com/example/websocket/controller/ChatControllerV2.java
@@ -1,13 +1,16 @@
 package com.example.websocket.controller;
 
 import com.example.websocket.domain.v2.Chat;
+import com.example.websocket.domain.v2.ChatType;
 import com.example.websocket.dto.v2.EnterAndExitRequestDto;
 import com.example.websocket.dto.v2.SendMessageRequestDto;
 import com.example.websocket.service.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,5 +41,18 @@ public class ChatControllerV2 {
             @DestinationVariable(value = "roomId") String roomId,
             @Payload EnterAndExitRequestDto request) {
         return chatService.exitRoom(roomId, request);
+    }
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @PostMapping("/chat")
+    public void test() {
+        Chat chat = Chat.builder()
+                .type(ChatType.EXIT)
+                .roomId("1")
+                .userName("sender2")
+                .content("sender2 님이 퇴장했습니다.")
+                .build();
+        rabbitTemplate.convertAndSend("chat.exchange", "room." + 1, chat); // 도착지 = chat.exchange/room.{roomId}
     }
 }
